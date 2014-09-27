f9e8be7dc44ab24abe9637ceabac23ffcc9c2bb7
Mamun Morshed <imroony@gmail.com>
2014-09-27 17:38:33 +0600

    Developed the theme and released a smoke version. Sections worked on -> home,about us, team, portfolio, contact. Work remains-> service section and image tunig, adding user experience, checking responsiveness and several other bugs and issues.

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+	var window_height = $(window).height(),
+		navbar_height = $('.navbar-adava').height();
+
+	// HOME HEIGHT
+	$( "#home" ).css( "height", window_height);
+	// margin-top of index search box
+	$( ".home-content" ).css( "margin-top", function() {
+		return (window_height - $( ".home-content" ).innerHeight()) /2;
+	} );
+
+	//jQuery to collapse the navbar on scroll
+	$(window).scroll(function() {
+	    if ($(".navbar").offset().top > window_height-navbar_height) {
+	        $(".navbar-fixed-top").addClass("navbar-dark");
+	    } else {
+	        $(".navbar-fixed-top").removeClass("navbar-dark");
+	    }
+	});
+
+});
